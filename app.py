0df10068913666029025f1170b31e4bc974cf459
pollux816 <133970707+pollux816@users.noreply.github.com>
2023-05-19 11:34:47 +0600

    Added app files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from models.users import User
+
+app = Flask(__name__)
+
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    register_date = request.form.get("register_date")
+    user = User(username, email, password, register_date)
+    return jsonify(user.get_all_users())
+
+
+app.run(debug=True)
---
+++ models/users.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class User:
+
+    def __init__(self, username, email, password, register_date):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.register_date = register_date
+        self.db = 'db.sqlite3'
+        self.connection = sqlite3.connect(self.db)
+        self.cursor = self.connection.cursor()
+
+    def get_all_users(self):
+        result = []
+        query = self.cursor.execute("SELECT * FROM users")
+        for data in query.fetchall():
+            result.append({
+                'id': data[0],
+                'username': data[1],
+                'email': data[2],
+                'password': data[3],
+                'register_date': data[4]
+            })
+        return result
